6178efcfabf11416f135e7aa0a27be47482ed37e
frayderMM <mezamorvelifrayder2005@gmail.com>
2025-11-22 08:27:44 -0500

    Initial commit

---
+++ app/src/main/java/dev/esan/pc02moviles2220014222200168/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.esan.pc02moviles2220014222200168
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.esan.pc02moviles2220014222200168.ui.theme.PC02MOVILES2220014222200168Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PC02MOVILES2220014222200168Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PC02MOVILES2220014222200168Theme {
+        Greeting("Android")
+    }
+}
